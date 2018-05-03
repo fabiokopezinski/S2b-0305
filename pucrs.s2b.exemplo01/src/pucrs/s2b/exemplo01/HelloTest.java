e1659a9fed3d0945886f3a0e3b51febe182862ba
Fabio Kopezinski <fabiokopezinski@gmail.com>
2018-05-03 14:21:33 -0300

    movendo o arquivo

---
+++ pucrs.s2b.exemplo01/src/pucrs/s2b/exemplo01/HelloTest.java
@@ -0,0 +1,24 @@
+package pucrs.s2b.exemplo01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void testSomar() {
+		
+		int expected=50;
+		int actual=Hello.somar(30,20);
+		assertEquals(expected, actual);
+		
+	}
+	@Test 
+	public void testSomar2() {
+		
+		assertEquals(0, Hello.somar(0, 0));
+		
+	}
+
+}
